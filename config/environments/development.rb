0c363f37aaac0954981212127d9d86010ed64fcb
Kévin Vinhas <kevin@vinhas.fr>
2021-02-24 21:53:05 +0100

    Removing Manifest, because it would also use graphiql in production
    Which we don't want, and is prevented by the fact that GraphiQL is
    installed only for certain envs.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Add GraphiQL assets only in development env
+  config.assets.precompile += ["graphiql/rails/application.js", "graphiql/rails/application.css"]
 end
