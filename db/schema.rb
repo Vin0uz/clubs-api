44be1c142ffd29c8dbfcae0306ed18dded564435
Kévin Vinhas <kevin@vinhas.fr>
2021-02-24 22:25:03 +0100

    Add pg_trgm extension for text research

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_210930) do
+ActiveRecord::Schema.define(version: 2021_02_24_211144) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "clubs", force: :cascade do |t|
